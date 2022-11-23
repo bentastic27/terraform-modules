8bfe3948d1dbc70cd3b8028b88f9ee6a764eb46e
Ben Healey <bentastic27@gmail.com>
2022-11-23 14:12:31 -0500

    specify nat_gateway_id instead of gwid to prevent constent changes with applies

--- k8s/eks-basic/networking.tf
+++ k8s/eks-basic/networking.tf
@@ -80,7 +80,7 @@ resource "aws_route_table" "private_subnet_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw.id
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   tags = {
