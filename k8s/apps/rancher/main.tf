a1ea7c12be0e5548d9a9e5fbd012144fc3468f3d
Ben Healey <bentastic27@gmail.com>
2022-11-25 13:11:10 -0500

    added optional nginx

--- k8s/apps/rancher/main.tf
+++ k8s/apps/rancher/main.tf
@@ -40,6 +40,30 @@ resource "helm_release" "cert_manager" {
   }
 }
 
+resource "helm_release" "ingress_nginx" {
+  count = var.install_nginx ? 1 : 0
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = var.nginx_version
+  namespace  = "ingress-nginx"
+  create_namespace = true
+  max_history = 10
+
+  set {
+    name = "controller.service.type"
+    value = var.nginx_service_type
+  }
+
+  dynamic "set" {
+    for_each = var.cert_manager_chart_set
+    content {
+      name = set.key
+      value = set.value
+    }
+  }
+}
+
 resource "helm_release" "rancher" {
   depends_on = [
     helm_release.cert_manager
@@ -52,6 +76,15 @@ resource "helm_release" "rancher" {
   namespace  = "cattle-system"
   create_namespace = true
   max_history = 10
+
+  dynamic "set" {
+    # hacky conditional dynamic block
+    for_each = var.install_nginx ? toset([1]) : toset([])
+    content {
+      name = "ingress.ingressClassName"
+      value = "nginx"
+    }
+  }
 
   dynamic "set" {
     for_each = var.rancher_chart_set
--- k8s/apps/rancher/variables.tf
+++ k8s/apps/rancher/variables.tf
@@ -26,3 +26,24 @@ variable "cert_manager_chart_set" {
   default = {
   }
 }
+
+variable "install_nginx" {
+  type = bool
+  default = false
+}
+
+variable "nginx_version" {
+  type = string
+  default = "4.0.18"  
+}
+
+variable "nginx_service_type" {
+  type = string
+  default = "LoadBalancer"
+}
+
+variable "nginx_chart_set" {
+  type = map(string)
+  default = {
+  }
+}
