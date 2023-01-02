fb511e73e681f858d20094c5ca8f15496db67aa8
Ben Healey <bentastic27@gmail.com>
2023-01-02 09:45:12 -0500

    fix for partial destroys

--- k8s/apps/rancher/output.tf
+++ k8s/apps/rancher/output.tf
@@ -4,5 +4,6 @@ output "rancher_url" {
 
 output "ingress_address" {
   description = "Rancher's ingress IP (Nginx ingress) or hostname (cloud provided LB)"
-  value = data.kubernetes_ingress_v1.rancher_address.status[0].load_balancer[0].ingress[0]
+  value = data.kubernetes_ingress_v1.rancher_address.status != null ? data.kubernetes_ingress_v1.rancher_address.status[0].load_balancer[0].ingress[0] : null
 }
+
